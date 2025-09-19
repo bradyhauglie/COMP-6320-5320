4a4a733918e1cbbddfdfa9873b49072980e39d0e
leoFes64 <94013524+leoFes64@users.noreply.github.com>
2025-09-18 20:31:48 -0500

    missing parenthesis

--- Programming-Assignment-1/client11c.c
+++ Programming-Assignment-1/client11c.c
@@ -19,7 +19,7 @@
 long long get_time_ms() {
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    return (long long)(tv.tv_sec * 1000000 + tv.tv_usec;
+    return (long long)(tv.tv_sec * 1000000 + tv.tv_usec;)
 }
 
 int main(int argc, char *argv[])
